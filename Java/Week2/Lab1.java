1a3ad72690bf6a97509a95fbb420935573e30169
TanByv <t2adbyv@gmail.com>
2022-10-02 20:46:40 +0300

    Added lab assignment

---
+++ Java/Week2/Lab1.java
@@ -0,0 +1,14 @@
+package Java.Week2;
+
+public class Lab1 {
+    public static void main(String[] args) // Main method
+
+    // Using println command to print the letters into a new line
+     {
+        System.out.println("MM       MM  EEEEEEE  FFFFFFF");
+        System.out.println("M  M    M M  E        F      ");
+        System.out.println("M   M  M  M  EEEEEEE  FFFFFFF");
+        System.out.println("M    M    M  E        F      ");
+        System.out.println("M    M    M  EEEEEEE  F      ");
+    }
+}
