cacc6ccf3eb21312c94a04f891b97f660e854c9b
TanByv <t2adbyv@gmail.com>
2022-10-09 19:52:31 +0300

    Complete Labworks2

--- Java/Week3/Labworks2.java
+++ Java/Week3/Labworks2.java
@@ -4,13 +4,18 @@ import java.util.Scanner;
 public class Labworks2 { 
     public static void main(String[] args) {
      
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
 
-        System.out.print("Enter the take off speed (m/s) and the acceleration (m/s^2) by leaving a space between them: ");
+            System.out.print("Enter the take off speed (m/s) and the acceleration (m/s^2) by leaving a space between them: ");  // Promt user for input
 
-        double TakeOff = input.nextDouble();
-        double Accel = input.nextDouble();
+            double Speed = input.nextDouble();  // Take both inputs at the same time
+            double Accel = input.nextDouble();
 
-        System.out.println("DEBUG: " + TakeOff + ", " + Accel);
+            double Runway = (Speed*Speed)/(2*Accel);    // Calculate the runway length in meters
+
+            System.out.print("The minimum runway length is ");  // Print the result while limiting the number printed to only 2 digits after the dot.
+            System.out.printf("%.2f", Runway);
+            System.out.print(" meters.");
+        }
     }
 }
