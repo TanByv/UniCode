5bf164a5e6d260ee8402ab6d76d6f1479cbecc02
TanByv <t2adbyv@gmail.com>
2022-10-09 19:41:18 +0300

    PoC

--- Java/Week3/Labworks2.java
+++ Java/Week3/Labworks2.java
@@ -1,8 +1,16 @@
 package Java.Week3;
 
+import java.util.Scanner;
 public class Labworks2 { 
     public static void main(String[] args) {
      
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the take off speed (m/s) and the acceleration (m/s^2) by leaving a space between them: ");
+
+        double TakeOff = input.nextDouble();
+        double Accel = input.nextDouble();
+
+        System.out.println("DEBUG: " + TakeOff + ", " + Accel);
     }
 }
