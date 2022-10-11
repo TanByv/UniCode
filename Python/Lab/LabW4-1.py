8342ed9461d23678735fefc44086ae6a096d7a5e
TanByv <t2adbyv@gmail.com>
2022-10-11 12:44:28 +0300

    End of LabW4

--- Python/Lab/LabW4-1.py
+++ Python/Lab/LabW4-1.py
@@ -4,12 +4,20 @@ inches = totalInches % 12
 print(feet, "feet and", inches, "inch")
 
 name = "Ali"
-print(name)
+print("\n" + name)
 
 string1 = "Introduction to Computer"
-print(string1[-8:])
+print("\n" + string1[-8:])
 
-print(string1.split(" ")[2])
+print("\n" + string1.split(" ")[2])
 
 repeatMe1 = "ta"
-print("Vatan " + repeatMe1 * 3 + "tan")
+print("\n" + "Vatan " + repeatMe1 * 3 + "tan")
+
+fullName = "Tan Bayav"
+print("\n" + fullName.split()[0] + " is your first name")
+print("\n" + fullName.split()[1] + " is your last name")
+
+
+userInput = input("\n" + "Enter something xd: ")
+print(userInput)
