a6e3a9a87780f54072e49d7b438c4b2d51766091
TanByv <t2adbyv@gmail.com>
2022-10-11 12:18:27 +0300

    Python related work

---
+++ Python/Lab/LabW4-1.py
@@ -0,0 +1,15 @@
+totalInches = 61
+feet = totalInches // 12
+inches = totalInches % 12
+print(feet, "feet and", inches, "inch")
+
+name = "Ali"
+print(name)
+
+string1 = "Introduction to Computer"
+print(string1[-8:])
+
+print(string1.split(" ")[2])
+
+repeatMe1 = "ta"
+print("Vatan " + repeatMe1 * 3 + "tan")
