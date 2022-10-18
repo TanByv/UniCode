fe1f60fe23317fb699653a4f2d9ffe47b58ef622
TanByv <t2adbyv@gmail.com>
2022-10-18 12:00:10 +0300

    Additions to LabW5

--- Python/Lab/LabW5-1.py
+++ Python/Lab/LabW5-1.py
@@ -3,3 +3,38 @@ print("{0:^5s}{1:<20s}{2:>6s}".format("Rank", "Player", "HR"))
 print("{0:^5n}{1:<20s}{2:>6n}".format(1, "Barry Bonds", 762.25))
 print("{0:^5n}{1:<20s}{2:>6n}".format(2, "Hank Aaron", 755.48))
 print("{0:^5n}{1:<20s}{2:>6n}".format(3, "Babe Ruth", 714.12))
+
+names = []
+grades = []
+
+var = input("\nEnter your first name: ")
+names.append(var)
+var = input("Enter your second name: ")
+names.append(var)
+var = input("Enter your third name: ")
+names.append(var)
+var = input("Enter your fourth name: ")
+names.append(var)
+var = input("Enter your fifth name: ")
+names.append(var)
+
+numberVar = float(input("\nEnter your first grade: "))
+grades.append(numberVar)
+numberVar = float(input("Enter your second grade: "))
+grades.append(numberVar)
+numberVar = float(input("Enter your third grade: "))
+grades.append(numberVar)
+numberVar = float(input("Enter your fourth grade: "))
+grades.append(numberVar)
+numberVar = float(input("Enter your fifth grade: "))
+grades.append(numberVar)
+
+average = sum(grades) / len(grades)
+
+print("{0:<20s}{1:<6s}".format("Student Names", "Grades"))
+print("{0:<20s}{1:<6n}".format(names[0], round(grades[0],2)))
+print("{0:<20s}{1:<6n}".format(names[1], round(grades[1],2)))
+print("{0:<20s}{1:<6n}".format(names[2], round(grades[2],2)))
+print("{0:<20s}{1:<6n}".format(names[3], round(grades[3],2)))
+print("{0:<20s}{1:<6n}".format(names[4], round(grades[4],2)))
+print("{0:<20s}{1:<6n}".format("Average", round(average,2)))
