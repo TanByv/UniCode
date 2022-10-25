b663f8e5930e2cdbede1cdc7b205f7141d46d4db
TanByv <t2adbyv@gmail.com>
2022-10-25 11:47:16 +0300

    Added better print statements

--- Python/Lab/LabW6-1.py
+++ Python/Lab/LabW6-1.py
@@ -5,9 +5,11 @@ with open(r'c:/users/bayavt/desktop/Numbers.txt', 'r') as fpp:
         x = line[:-1]
         NumbersRead.append(eval(x))
         
-print(NumbersRead)
+print("Full list:",NumbersRead)
 
 NumbersSorted = [i for i in NumbersRead if i > 50]
 
-print(NumbersSorted)
-print(sum(NumbersSorted))
+print("\nList with numbers greater than 50:",NumbersSorted)
+print("\nSum of above list:",sum(NumbersSorted))
+
+fpp.close()
