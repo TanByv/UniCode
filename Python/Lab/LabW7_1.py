a32e9d81488791fdb54c044940fbef41876fa6b0
TanByv <t2adbyv@gmail.com>
2022-11-01 11:22:29 +0300

    Commit

---
+++ Python/Lab/LabW7_1.py
@@ -0,0 +1,13 @@
+def convert_celsius_to_fahrenheit(celsius):
+    fahrenheit = (float(celsius) * 9 / 5 + 32)
+    return fahrenheit
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    celsius = (float(fahrenheit - 32) * 5 / 9)
+    return celsius
+
+cel = eval(input("Enter celsius: "))
+print(convert_celsius_to_fahrenheit(cel), "fahrenheit")
+
+fer = eval(input("Enter fahrenheit: "))
+print(convert_fahrenheit_to_celsius(fer), "celsius")
