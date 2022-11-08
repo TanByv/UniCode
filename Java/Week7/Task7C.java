673f86dd7b88dfadb1fabfa467e5cc4e858dc198
TanByv <t2adbyv@gmail.com>
2022-11-08 19:56:36 +0300

    Finish 7C and 7D

--- Java/Week7/Task7C.java
+++ Java/Week7/Task7C.java
@@ -1,5 +1,22 @@
 package Java.Week7;
 
 public class Task7C {
-    
+    public static void main(String[] args) {
+        int i = 0; // Define counter variable
+        while (i < 3) { // Start while loop if counter is less than 3
+            int r1 = (int)(Math.random() * (15 - 3)) + 3; // Generate number between 15 and 3
+            System.out.println(r1 + "! = " + factorial(r1) + "\n"); // Print the factorial and it's solution
+            i++;
+        }
+    }
+
+    public static int factorial (int num) { // Fucntion for calculating factorial
+        int product = 1;
+
+        while (num != 1) {  // if num isn't equal to 1, do this in a loop
+            product = product * num;
+            num = num - 1;
+        }
+        return product;
+    }
 }
--- Java/Week7/Task7D.java
+++ Java/Week7/Task7D.java
@@ -1,5 +1,30 @@
 package Java.Week7;
 
+import java.util.Scanner;
 public class Task7D {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter lower limit: "); // Take lower limit
+            int lowerL = input.nextInt();
+            System.out.print("Enter upper limit: "); // Take upper limit
+            int upperL = input.nextInt();
+            System.out.print("Enter the increment: "); // Take the increment
+            int increment = input.nextInt();
+            
+            System.out.println("Answer: " + (sumUp(lowerL, upperL, increment))); // Call the function using using user inputs
+
+        }  catch (Exception e) { // Catching the exception to prevent crash in case of input not being a valid number
+            System.out.println("\nPlease enter a valid number.");
+        } 
+    }
     
+    public static int sumUp(int num1, int num2, int num3) {
+        int sum = 0;
+
+        while ((num1 <= num2)) { // If num1 is equal or smaller than num2 keep doing the loop
+            sum += num1;
+            num1 += num3;
+        }
+        return sum;
+    }
 }
