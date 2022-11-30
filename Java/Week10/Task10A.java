f199ae64fe790b6b85102ef950d3a40a8266e7ad
TanByv <t2adbyv@gmail.com>
2022-11-30 19:41:53 +0300

    Fix typo

--- Java/Week10/Task10A.java
+++ Java/Week10/Task10A.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task10A {
     public static void main(String[] args) {
         
-        int[] hahaXD = new int[20];   // Create an array named hahaXD with lenght of 20 and data type set to integer as requested
+        int[] hahaXD = new int[20];   // Create an array named hahaXD with length of 20 and data type set to integer as requested
 
         try (Scanner input = new Scanner(System.in)) {
 
