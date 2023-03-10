d091aa9b001127363ec9cc86abf77ee9d5ad5433
TanByv <t2adbyv@gmail.com>
2023-03-10 11:30:57 +0300

    Inclass1 complete

--- MIPS/Week1/Inclass1.asm
+++ MIPS/Week1/Inclass1.asm
@@ -1,11 +1,38 @@
-addi $t0, $zero, 0      # $t0 = i = 0
-la $t1, b               # $t1 = array b
-la $t2, c               # $t2 = array c
-la $t3, a               # $t3 = array a
+.data
+    # Array with 5 integer values
+    a:
+        .word 48, 29, 10, 18, 22
+    b:
+        .word 36, 24, 78, 17, 19
+    c:
+        .word 1, 0, 0, 0, 0	
+    str:
+    .asciiz "The result is "
 
-lw $s0, 0($t1)          # $s0 = b[i]
-lw $s1, 0($t2)          # $s1 = c[i]
-add $s2, $s0, $s1       # $s2 = b[i] + c[i]
+.text
+    main:
+        addi $t0, $zero, 0      # $t0 = i = 0
+        la $t1, b               # $t1 = array b
+        la $t2, c               # $t2 = array c
+        la $t3, a               # $t3 = array a
 
-# Store result in a[i]
-sw $s2, 0($t3)          # a[i] = $s2
+        lw $s0, 4($t1)          # $s0 = b[i]
+        lw $s1, 0($t2)          # $s1 = c[i]
+        add $s2, $s0, $s1       # $s2 = b[i] + c[i]
+
+        # Store result in a[i]
+        sw $s2, 0($t3)          # a[i] = $s2
+
+        # Print the message
+        li $v0, 4      # Load system call code for print string
+        la $a0, str    # Load address of message into $a0
+        syscall  
+
+        # Print the result
+        li $v0, 1
+        lw $a0, a
+        syscall
+
+    Exit:	# Exit program
+        li $v0, 10
+        syscall
