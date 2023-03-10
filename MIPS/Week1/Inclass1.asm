857a5d407edf0bd02b510605b471c422688ad7a7
TanByv <t2adbyv@gmail.com>
2023-03-10 11:17:49 +0300

    Update to Inclass1

--- MIPS/Week1/Inclass1.asm
+++ MIPS/Week1/Inclass1.asm
@@ -1,10 +1,8 @@
-# Initialize variables
 addi $t0, $zero, 0      # $t0 = i = 0
-la $t1, b               # $t1 = address of array b
-la $t2, c               # $t2 = address of array c
-la $t3, a               # $t3 = address of array a
+la $t1, b               # $t1 = array b
+la $t2, c               # $t2 = array c
+la $t3, a               # $t3 = array a
 
-# Load values from arrays and perform addition
 lw $s0, 0($t1)          # $s0 = b[i]
 lw $s1, 0($t2)          # $s1 = c[i]
 add $s2, $s0, $s1       # $s2 = b[i] + c[i]
