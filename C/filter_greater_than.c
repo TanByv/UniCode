7ccb44fa22c624b71be54fab7df2058bcabdafa2
TanByv <t2adbyv@gmail.com>
2023-11-27 13:04:08 +0300

    Add comments

--- C/filter_greater_than.c
+++ C/filter_greater_than.c
@@ -4,12 +4,11 @@
 
 #define ARR_SIZE 10
 
-// Tan Bayav - 042201156
-void filter_greater_than(int arr[], int* pivot, int filtered[], int* size) {
+void filter_greater_than(int arr[], int* pivot, int filtered[], int* size) { // Define the actual function
 
   *size = 0;
 
-  for (int i = 0; i < ARR_SIZE; i++) {
+  for (int i = 0; i < ARR_SIZE; i++) { // Iterate through the array and count amount of filtered entries
     if (arr[i] > *pivot) {
       filtered[*size] = arr[i];
       (*size)++; 
@@ -19,19 +18,19 @@ void filter_greater_than(int arr[], int* pivot, int filtered[], int* size) {
 
 int main() {
 
-  int arr[ARR_SIZE] = {1, 5, 3, 8, 2, 9, 6, 4, 7, 0};
+  int arr[ARR_SIZE] = {1, 5, 3, 8, 2, 9, 6, 4, 7, 0}; // Define the array
 
   srand(time(NULL));
-  int* pivot = &arr[rand() % ARR_SIZE];
+  int* pivot = &arr[rand() % ARR_SIZE]; // Randomize the pivot
 
   printf("Pivot: %d\n", *pivot);
 
-  int filtered[ARR_SIZE];
+  int filtered[ARR_SIZE]; // Initialize the filtered array
   int f_size = 0;
 
-  filter_greater_than(arr, pivot, filtered, &f_size);
+  filter_greater_than(arr, pivot, filtered, &f_size); // Call the function to populate the filtered array
 
-  printf("Filtered Array: ");
+  printf("Filtered Array: "); // Print the filtered array
   for (int i = 0; i < f_size; i++) {
     printf("%d ", filtered[i]); 
   }
--- C/triangles.c
+++ C/triangles.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 
-// Tan Bayav - 042201156
 static int equi_cnt = 0, iso_cnt = 0, sca_cnt = 0;
 
 void classify_triangles(float side1, float side2, float side3) {
-  if(side1 == side2 && side2 == side3) {
+  if(side1 == side2 && side2 == side3) { // Count the equi triangles
     equi_cnt++; 
   }
-  else if(side1 == side2 || side2 == side3 || side1 == side3) {
+  else if(side1 == side2 || side2 == side3 || side1 == side3) { // Count the iso triangles, using else if simplifies the check
     iso_cnt++;
   }
-  else {
+  else { // Count sca triangles, which would be the triangles didn't get caught by the earlier checks
     sca_cnt++; 
   }
   
-  int sum = side1 + side2 + side3;
+  int sum = side1 + side2 + side3; // Check if the triangle is small
   if(sum <= 12) {
     printf("You entered a small triangle.\n\n"); 
   }
 }
 
 int main() {
-  float s1, s2, s3;
+  float s1, s2, s3; // Initialize variables
   
   while(1) {
-    printf("Enter triangle sides (or -1 -1 -1 to quit): ");
+    printf("Enter triangle sides (or -1 -1 -1 to quit): "); // Ask for input
     scanf("%f%f%f", &s1, &s2, &s3);
-    if(s1 == -1 && s2 == -1 && s3 == -1)
+    if(s1 == -1 && s2 == -1 && s3 == -1) // If input is -1 -1 -1, stop the loop and show the results
       break;
       
     classify_triangles(s1, s2, s3);
   }
   
-  printf("Equilateral triangles: %d\n", equi_cnt);
+  printf("\nEquilateral triangles: %d\n", equi_cnt); // Print results
   printf("Isosceles triangles: %d\n", iso_cnt); 
   printf("Scalene triangles: %d\n", sca_cnt);
   
